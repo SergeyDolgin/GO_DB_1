40f18d58277edb6869559282c993fa2fb67509ad
SergeyDolgin <112004867+SergeyDolgin@users.noreply.github.com>
2024-04-19 22:00:37 +0700

    +

---
+++ internal/database/users/repository.go
@@ -0,0 +1,48 @@
+package users
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
+
+	"gitlab.com/robotomize/gb-golang/homework/03-01-umanager/internal/database"
+)
+
+func New(userDB *pgx.Conn, timeout time.Duration) *Repository {
+	return &Repository{userDB: userDB, timeout: timeout}
+}
+
+type Repository struct {
+	userDB  *pgx.Conn
+	timeout time.Duration
+}
+
+func (r *Repository) Create(ctx context.Context, req CreateUserReq) (database.User, error) {
+	var u database.User
+
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+
+	// implement me
+	return u, nil
+}
+
+func (r *Repository) FindByID(ctx context.Context, userID uuid.UUID) (database.User, error) {
+	var u database.User
+
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+	// implement me
+	return u, nil
+}
+
+func (r *Repository) FindByUsername(ctx context.Context, username string) (database.User, error) {
+	var u database.User
+
+	ctx, cancel := context.WithTimeout(ctx, r.timeout)
+	defer cancel()
+	// implement me
+	return u, nil
+}
